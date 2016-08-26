294891ed6fdc42a58a2da50101b81cf08eb63353
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-08-26 08:25:13 -0700

    add pizza img and remove two img

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
 // Back end
+var Pizza = function () {
+  this.size = "";
+  this.toppings = new array;
+  this.type = "";
+};
+
+Pizza.prototype.Total = function() {
+  var total = 0;
+
+  total += this.sizeCost();
+  total += this.toppingsCost();
+
+  return total;
+};
+
+
+Pizza.prototype.sizeCost = function() {
+  // body...
+  return cost;
+};
+
+Pizza.prototype.toppingsCost = function() {
+  // body...
+  return cost;
+};
+
+Pizza.prototype.addToppings = function(toppings) {
+  this.toppings.push(toppings);
+};
+
+Pizza.prototype.removeToppings = function(toppings) {
+
+};
 
 
 // front end
